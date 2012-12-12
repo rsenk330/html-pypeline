ced8c5476fb94ce2b19a8b0f9e7455d3e6ebf8c3
Ryan Senkbeil <rsenk330@gmail.com>
2012-12-12 17:52:39 -0600

    Add comments to tests.
    Remove nose config... It needs to be in the home directory.

--- pypeline/tests/meta_tests.py
+++ pypeline/tests/meta_tests.py
@@ -4,7 +4,9 @@ import pypeline
 
 class MetaTests(unittest.TestCase):
     def test_version(self):
+        """Test for accurate version number"""
         self.assertEqual(pypeline.__version__, '0.1')
 
     def test_title(self):
+        """Test to see if the title is present"""
         self.assertEqual(pypeline.__title__, 'html-pypeline')
