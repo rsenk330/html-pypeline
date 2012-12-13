d12c10e6bedf675072e98661c74ee81ed91ef9cc
Ryan Senkbeil <rsenk330@gmail.com>
2012-12-12 20:31:19 -0600

    Add some more deps.

--- pypeline/filters/tests/markdown_tests.py
+++ pypeline/filters/tests/markdown_tests.py
@@ -3,6 +3,13 @@ import pypeline
 from pypeline.utils import unittest
 
 class MarkdownTests(unittest.TestCase):
+    def setUp(self):
+        filters = [
+            pypeline.filters.markdown()
+        ]
+
+        self.renderer = pypeline.Pipeline(filters)
+
     def test_markdown_render(self):
         """Tests to make sure markdown is rendered with the markdown filter"""
         markdown = """\
@@ -10,11 +17,6 @@ class MarkdownTests(unittest.TestCase):
 
 This is test markdown. It **should** render in *HTML*.
 """
-        filters = [
-            pypeline.filters.markdown()
-        ]
-
-        renderer = pypeline.Pipeline(filters)
-        html = renderer.render(markdown)
+        html = self.renderer.render(markdown)
 
         self.assertEqual(html, "<h1>Test Header</h1>\n\n<p>This is test markdown. It <strong>should</strong> render in <em>HTML</em>.</p>\n")
