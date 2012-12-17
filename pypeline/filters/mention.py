705fb625d19dac7cce3ec400b234e4a40ba8f3dc
Ryan Senkbeil <rsenk330@gmail.com>
2012-12-17 15:23:10 -0600

    Add more tests for @mentions.

--- pypeline/filters/mention.py
+++ pypeline/filters/mention.py
@@ -4,11 +4,31 @@ from lxml.html import fromstring, tostring
 from lxml.etree import SubElement
 
 class Mention(object):
+    """Replaces `@mention`'s with a URL pointing to the user's profile.
+
+    The URL can be configured by setting the ``user_url`` key in the `context`.
+    By default, it is set to `/users/{username}. ``{username}`` will always
+    be replaced by everything after the `@` symbol in the `@mention`.
+
+    Ignored usernames can be configured by setting the ``ignore_username``
+    key in the `context`. By default, it ignores `mention` and `mentioning`.
+    This should always be an iterable.
+
+    The class applied to `@mention` links defaults to ``mention``. This can
+    be changed by setting the ``class`` key in the `context`.
+
+    """
     PARENT_IGNORES = ('pre', 'code', 'a')
     MENTION_RE = r'\B@(?P<username>\w+)'
 
     def __init__(self, context={}):
-        self.context = context
+        self.context = {
+            'user_url': '/users/{username}',
+            'ignore_username': ['mention', 'mentioning'],
+            'class': 'mention'
+        }
+
+        self.context.update(context)
 
     def __call__(self, content):
         fragment = fromstring(content)
@@ -43,10 +63,11 @@ class Mention(object):
         """Generator that yields a list of all `@mentions`."""
         text = element.text
 
-        mentions = re.findall(self.MENTION_RE, text)
+        mentions = set(re.findall(self.MENTION_RE, text))
 
         for mention in mentions:
-            yield "@{0}".format(mention)
+            if mention not in self.context['ignore_username']:
+                yield "@{0}".format(mention)
 
     def _add_mention_links(self, element):
         """Replaces `@mentions` with links
@@ -56,11 +77,13 @@ class Mention(object):
         """
         text = tostring(element)
         for mention in self._mentioned_logins(element):
+            url = self.context['user_url'].format(username=mention[1:])
+
             anchor = SubElement(element, "a")
-            anchor.attrib['href'] = '#'
-            anchor.attrib['class'] = 'mention'
+            anchor.attrib['href'] = url
+            anchor.attrib['class'] = self.context['class']
             anchor.text = mention
 
-            text = text.replace(mention, tostring(anchor))
+            text = re.sub(r'{0}\b'.format(mention), tostring(anchor), text)
 
         return fromstring(text)
--- pypeline/filters/tests/mention_tests.py
+++ pypeline/filters/tests/mention_tests.py
@@ -6,31 +6,98 @@ from pypeline.utils import unittest
 
 class MentionTests(unittest.TestCase):
     def setUp(self):
-        filters = [
-            pypeline.filters.mention()
-        ]
+        self.renderer = pypeline.Pipeline(self._build_filters())
 
-        self.renderer = pypeline.Pipeline(filters)
+        self.html_fragment = """\
+<div>
+    <h1>Testing</h1>
+    <p>Blah</p>
+    <p>@mentionee to a @user and @mention</p>
+</div>"""
+
+    def _build_filters(self, context={}):
+        return [
+            pypeline.filters.mention(context)
+        ]
 
     def _string_eql(self, expected, filtered):
         return LXMLOutputChecker().check_output(expected, filtered, 0)
 
     def test_mention_replace(self):
-        """Tests to see if @mentions are replaces with URLs"""
-        html_fragment = """\
-<div>
-    <h1>Testing</h1>
-    <p>Blah</p>
-    <p>@mention to a @user</p>
-</div>"""
+        """Tests to see if @mentions are replaced with URLs"""
         expected = """\
 <div>
     <h1>Testing</h1>
     <p>Blah</p>
-    <p><a href="#" class="mention">@mention</a> to a <a href="#" class="mention">@user</a></p>
+    <p><a href="/users/mentionee" class="mention">@mentionee</a> to a <a href="/users/user" class="mention">@user</a>  and @mention</p>
 </div>"""
 
-        filtered = self.renderer.render(html_fragment)
+        filtered = self.renderer.render(self.html_fragment)
+
+        self.assertNotEqual(filtered, self.html_fragment)
+        self.assertTrue(self._string_eql(expected, filtered), "{0} != {1}".format(filtered, expected))
+
+    def test_mention_replace_user_url(self):
+        renderer = pypeline.Pipeline(self._build_filters({'user_url': 'http://localhost:5000/users/{username}'}))
+        expected = """\
+<div>
+    <h1>Testing</h1>
+    <p>Blah</p>
+    <p><a href="http://localhost:5000/users/mentionee" class="mention">@mentionee</a> to a <a href="http://localhost:5000/users/user" class="mention">@user</a>  and @mention</p>
+</div>"""
+
+        filtered = renderer.render(self.html_fragment)
+
+        self.assertNotEqual(filtered, self.html_fragment)
+        self.assertTrue(self._string_eql(expected, filtered), "{0} != {1}".format(filtered, expected))
+
+    def test_mention_set_class(self):
+        renderer = pypeline.Pipeline(self._build_filters({'class': 'user-mention'}))
+        expected = """\
+<div>
+    <h1>Testing</h1>
+    <p>Blah</p>
+    <p><a href="/users/mentionee" class="user-mention">@mentionee</a> to a <a href="/users/user" class="user-mention">@user</a>  and @mention</p>
+</div>"""
+
+        filtered = renderer.render(self.html_fragment)
+
+        self.assertNotEqual(filtered, self.html_fragment)
+        self.assertTrue(self._string_eql(expected, filtered), "{0} != {1}".format(filtered, expected))
+
+    def test_mention_ignore_username(self):
+        """Tests to see if @mentions are replaced with URLs"""
+        renderer = pypeline.Pipeline(self._build_filters({'ignore_username': ['mentionee']}))
+        expected = """\
+<div>
+    <h1>Testing</h1>
+    <p>Blah</p>
+    <p>@mentionee to a <a href="/users/user" class="mention">@user</a> and <a href="/users/mention" class="mention">@mention</a></p>
+</div>"""
+
+        filtered = renderer.render(self.html_fragment)
+
+        self.assertNotEqual(filtered, self.html_fragment)
+        self.assertTrue(self._string_eql(expected, filtered), "{0} != {1}".format(filtered, expected))
+
+    def test_mention_duplicates(self):
+        """Tests to see if @mentions are replaced with URLs"""
+        renderer = pypeline.Pipeline(self._build_filters({'ignore_username': ['mentionee']}))
+        html_fragment = """\
+<div>
+    <h1>Testing</h1>
+    <p>Blah</p>
+    <p>@mentionee to a @user and @user again and @mention</p>
+</div>"""
+
+        expected = """\
+<div>
+    <h1>Testing</h1>
+    <p>Blah</p>
+    <p>@mentionee to a <a href="/users/user" class="mention">@user</a> and <a href="/users/user" class="mention">@user</a> again and <a href="/users/mention" class="mention">@mention</a></p>
+</div>"""
+
+        filtered = renderer.render(html_fragment)
 
         self.assertNotEqual(filtered, html_fragment)
         self.assertTrue(self._string_eql(expected, filtered), "{0} != {1}".format(filtered, expected))
